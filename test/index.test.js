32a41b85e58c5f5133bffe866f2e0d724d229417
Samuel Gaborieau <sam.gabor@hotmail.com>
2021-11-10 19:14:03 +0100

    upgrade endpoint test file

--- test/index.test.js
+++ test/index.test.js
@@ -3,5 +3,26 @@
  * this file should be execute with > mocha ./test/index.test.js
  */
 require('jsdom-global')();
+
+require('./parse-value.test');
+require('./ui2camelcase.test');
+
 require('./create-player-list.test');
 require('./extract-player-list-item.test');
+
+require('./create-player-item.test');
+require('./get-player-core.test');
+
+
+require('./extract-header-player-item.test');
+require('./extract-teams-player-item.test');
+
+require('./extract-player-skills/extract-player-ballskills.test');
+require('./extract-player-skills/extract-player-shooting.test');
+require('./extract-player-skills/extract-player-passing.test');
+require('./extract-player-skills/extract-player-defence.test');
+require('./extract-player-skills/extract-player-physical.test');
+require('./extract-player-skills/extract-player-mental.test');
+require('./extract-player-skills/extract-player-goalkeeper.test');
+
+require('./extract-player-item.test');
